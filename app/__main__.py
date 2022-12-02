5845dbe67c4b0c764525be9f53f6708d1701fca5
Kuba Bak <kubabak@gmail.com>
2022-12-02 14:49:56 +0100

    add docker compose for postgre

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,6 @@
 if __name__ == "__main__":
     print("Placeholder for main standalone")
+    # start backend app
+    print("application up and running")
 else:
     print("Placeholder for imported")
---
+++ app/db/plant.py
@@ -0,0 +1,1 @@
+# obiekt dla modelu plant
